8b80fca570b004724ca7328d5240336f7c02e2e6
Silvio Santoriello <silviosanto6605@gmail.com>
2025-04-08 22:15:31 +0200

    aggiunta funzione ADD di base, da implementare la logica di controllo per spazio libero

--- src/LinkedList.s
+++ src/LinkedList.s
@@ -1,18 +1,15 @@
 .data
 
 listInput: .string 'ciaoprova123'
-HEAD_PTR: .word 0 #puntatore alla testa della lista
-LAST_PTR: .word 0 #puntatore all'ultimo elemento della lista
-BASE_ADDR: .word 0x10000000 #parto a salvare la lista da questo indirizzo
-
-
-
+START_ADDR: .word 0x10000000 #indirizzo di inizio dei dati statici
+END_ADDR: .word 0x10000096 #indirizzo di fine dei dati (START_ADDR + 30*5 siccome posso avere max 30 nodi da 5 byte l'uno)
 
 .text
 
-la s1,listInput #salvo la testa della stringa di input che passerò alla funzione di parsing
-lw s2,HEAD_PTR
-lw s3,LAST_PTR
+la s1,listInput #salvo la testa della stringa di input che passera alla funzione di parsing
+li s2,0 #HEAD_PTR puntatore alla testa della lista
+li s3,0 #LAST_PTR puntatore all'ultimo elemento della lista
+
 li s4,0 #counter dei nodi
 # jal PARSING  #devo implementare la logica di parsing
 
@@ -26,30 +23,36 @@ jal ADD           # Terzo nodo
 
 
 
+ADD: #parametro a0                             
 
-ADD: #ADD(a0->char)
-    la t0, BASE_ADDR #carico l'indirizzo di BASE_ADDR
-    lw t1, 0(t0) #contenuto di BASE_ADDR caricato in t1
-    
-    sb a0, 0(t1) #scrivo nel primo byte (cioé DATA) il carattere passato come parametro
-    sw zero, 1(t1) #PAHEAD= 0x00000000 cioé zero
-    
-    bgt s4,zero,normal_ADD #tratto la prima ADD diversamente dalle altre
-    
-    mv s2,t1 #HEAD_PTR salvato nella variabile globale
-    mv s3,t1 #LAST_PTR salvato nella variabile globale
-    j updateADD
-    
-    normal_ADD:
-        sw t1,1(s3) #Collego l'ultimo nodo a quello attuale (cioé )
-        mv s3,t1 #aggiorno LAST_PTR
-    
-    updateADD:
-        addi t1,t1,5 # aumento il BASE_ADDR di 5
-        sw t1, 0(t0) #
-        addi s4,s4,1 #ho aggiunto un nodo, incremento il contatore
-        jr ra
+ #chiamata alla funzione individua prox indirizzo
+ # da gestire lo stack
+ # jal find_next_free_addr  #ritorna in a1 il prossimo indirizzo di scrittura
+ 
+ 
+sb a0,0(a1) #scrivo il dato,parametro dÃ¬ ADD, nel campo DATA del nodo
+sw zero,1(a1) #rendo null il puntatore del nodo inserito
 
+beq zero,s4,firstADD  #e' il primo nodo? NO: proseguo, SÃŒ: salto
+
+sw a1,1(s3) #salvo in PAHEAD del nodo prima l'indirizzo dell'ultimo
+add s3,zero,a1 #salvo in LAST_PTR l'indirizzo dell'ultimo nodo
+addi s4,s4,1 # aumento counter nodi
+jr ra
+
+
+
+
+ #caso in cui il nodo aggiunto sia il primo
+ firstADD:
+     add s2,zero,a1 #HEAD_PTR = indirizzo ultimo nodo
+     add s3,zero,a1 #LAST_PTR = indirizzo ultimo nodo
+     addi s4,s4,1 # aumento counter nodi
+     jr ra
+     
+ 
+
+        
      
     
         
