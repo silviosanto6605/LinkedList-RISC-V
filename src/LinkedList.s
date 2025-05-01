71ed5332583409178f38e52579f63833ecb2b43e
Silvio Santoriello <silviosanto6605@gmail.com>
2025-05-02 00:43:37 +0200

    Ora ADD e funzione per ricerca spazio libero funzionano più o meno correttamente

--- src/LinkedList.s
+++ src/LinkedList.s
@@ -1,7 +1,6 @@
 .data
 
-listInput: .string 'ciaoprova123'
-NEXT_FREE_ADDR: .word 0x10000000 #indirizzo di inizio dei dati statici
+listInput: .string "ciaoprova123"
 
 
 .text
@@ -9,70 +8,114 @@ NEXT_FREE_ADDR: .word 0x10000000 #indirizzo di inizio dei dati statici
 la s1,listInput #salvo la testa della stringa di input che passera alla funzione di parsing
 li s2,0 #HEAD_PTR puntatore alla testa della lista
 li s3,0 #LAST_PTR puntatore all'ultimo elemento della lista
-
 li s4,0 #counter dei nodi
-la s5,NEXT_FREE_ADDR #carico NEXT_FREE_ADDR
+li s5,0x10000000 #carico NEXT_FREE_ADDR, indirizzo di inizio dei dati statici
 # jal PARSING  #devo implementare la logica di parsing
 
-li a0, 101
-jal ADD           # Primo nodo
-li a0, 102
-jal ADD           # Secondo nodo
-li a0, 103
-jal ADD           # Terzo nodo
+li a0,83 #S
+jal ADD
+
+li a0,105 #i
+jal ADD
+
+li a0,108 #l
+jal ADD
+
+li a0,118 #v
+jal ADD
+
+li a0,105 #i
+jal ADD
+
+li a0,111 #o
+    jal ADD
 
 
+li t4,20
 
-
-ADD: #parametro a0                             
-
- #chiamata alla funzione individua prox indirizzo
- # da gestire lo stack
- 
-jal find_next_free_addr  #ritorna in a1 il prossimo indirizzo di scrittura
- 
- 
-sb a0,0(a1) #scrivo il dato,parametro dì ADD, nel campo DATA del nodo
-sw zero,1(a1) #rendo null il puntatore del nodo inserito
-
-beq zero,s4,firstADD  #e' il primo nodo? NO: proseguo, SÌ: salto
-
-sw a1,1(s3) #salvo in PAHEAD del nodo prima l'indirizzo dell'ultimo
-add s3,zero,a1 #salvo in LAST_PTR l'indirizzo dell'ultimo nodo
-addi s4,s4,1 # aumento counter nodi
-jr ra
-
-
-
-
-
- #caso in cui il nodo aggiunto sia il primo
- firstADD:
-     add s2,zero,a1 #HEAD_PTR = indirizzo ultimo nodo
-     add s3,zero,a1 #LAST_PTR = indirizzo ultimo nodo
-     addi s4,s4,1 # aumento counter nodi
-     jr ra
+loop_test:
+    beqz t4,end
+    
+    li a0,111 #o
+    jal ADD
+    addi t4,t4,-1
+    j loop_test
      
- 
+
+end:
+    li a7,10
+    ecall
+
+
+
+
+
+
+ADD: # parametro in a0 (DATA)
+
+    addi sp,sp,-4       
+    sw ra,0(sp)                # Salva il return address nella stack
+
+   jal find_next_free_addr    # Ritorna in a1 il prossimo indirizzo di scrittura 
+    
+   sb a0,0(a1)                # Scrivi DATA nel primo byte del nodo
+   addi a1,a1,1               # in questo modo vado a prendere il byte successivo
+   sw zero,0(a1)              # PAHEAD = 0 (ultimo nodo)
+   
+   addi a1,a1,-1              #siccome avevo incrementato di uno per scrivere, devo rifixare
+
+    beq zero,s4,firstADD       # Se è il primo nodo, salta
+    sw a1,1(s3)                # Aggiorna PAHEAD del nodo precedente
+    mv s3,a1                   # Aggiorna LAST_PTR
+    addi s4,s4,1               # Incrementa il contatore nodi
+    j end_ADD
+
+firstADD:
+    mv s2,a1                   # HEAD_PTR = indirizzo nuovo nodo
+    mv s3,a1                   # LAST_PTR = indirizzo nuovo nodo
+    addi s4,s4,1               # Incrementa il contatore nodi
+
+end_ADD:
+    lw ra,0(sp)
+    addi sp,sp,4
+    jr ra
+
+
 
         
-find_next_free_addr:
-    lb t1,0(s5) #byte di DATA
-    lw t2,1(s5) #4B di PAHEAD
+find_next_free_addr: #ritorno in a1
+    li t0,5 #devo trovare 5 byte
+
+check_bytes:
+    lb t1,0(s5) #leggo il byte corrente
+    bnez t1,reset_counter # se non trovo zero vuol dire che non c'è spazio
     
-    or t3,t1,t2 #Sono tutti 0? cioé: è libero?
-    bne t3,zero,no_space #se no aumenta
-    mv a1,s5
-    ret
+    addi t0,t0,-1 #decremento il contatore di ciclo (4 byte da analizzare, 3 byte da analizzare) 
+    beqz t0,found
+
+    addi s5,s5,1 # vado nel byte successivo
+    j check_bytes #ricontrollo
     
-    no_space:
+    
+    reset_counter:
         addi s5,s5,1 #analizzo a partire dal bit successivo
+        li t0,5
         j find_next_free_addr #continua finche non trovi 5 byte liberi
     
- 
+    found:
+        addi s5,s5,-4 # siccome sono andato avanti di 4 byte, vado indietro e ritorno
+        mv a1,s5
+        addi s5,s5,5 #siccome ora 5 byte sono occupati, faccio un grande balzo in avanti
         
-         
-    
+        mv t6,a1 #DEBUG: MOSTRA INDIRIZZO
+        li a7,34
+        ecall
         
-    
+        ret
+
+
+
+
+
+
 
