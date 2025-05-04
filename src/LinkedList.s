1c58eb16e1557be26741e7209d5fe13dc6f6d35b
Silvio Santoriello <silviosanto6605@gmail.com>
2025-05-04 20:20:39 +0200

    Aggiunta funzione di cancellazione

--- src/LinkedList.s
+++ src/LinkedList.s
@@ -22,6 +22,12 @@ jal ADD
 li a0,108 #l
 jal ADD
 
+li a0,108 #l
+jal ADD
+
+li a0,108 #l
+jal ADD
+
 li a0,118 #v
 jal ADD
 
@@ -45,6 +51,18 @@ loop_test:
 
 end:
     jal PRINT
+    li a0,111
+    jal DEL
+    li a0,108
+    jal DEL
+    jal PRINT
+    
+    li a0,111 #o
+    jal ADD
+    jal PRINT
+    
+    
+    
     li a7,10
     ecall
 
@@ -122,59 +140,131 @@ ADD: # parametro in a0 (DATA)
     addi s4,s4,1               # Incrementa il contatore nodi
     j end_ADD
 
-firstADD:
-    mv s2,a1                   # HEAD_PTR = indirizzo nuovo nodo
-    mv s3,a1                   # LAST_PTR = indirizzo nuovo nodo
-    addi s4,s4,1               # Incrementa il contatore nodi
+    firstADD:
+        mv s2,a1                   # HEAD_PTR = indirizzo nuovo nodo
+        mv s3,a1                   # LAST_PTR = indirizzo nuovo nodo
+        addi s4,s4,1               # Incrementa il contatore nodi
 
-end_ADD:
-    lw ra,0(sp)
-    addi sp,sp,4
-    jr ra
+    end_ADD:
+        lw ra,0(sp)
+        addi sp,sp,4
+        jr ra
 
+
+
+DEL:
+     #carattere da cercare in a0
+
+    addi sp,sp,-4
+    sb ra, 0(sp) # salvo ra
+    
+    beqz s2, end_DEL #controllo se ci sono nodi
+
+
+    check_head_deletion: #il carattere è nella testa?
+
+        lb t0,0(s2) #carico il DATA del nodo HEAD 
+        bne t0,a0, del_find_next # controlla se eliminarla in quanto è presente il carattere parametro da eliminare
+
+        addi t1,s2,1 # mi allineo al PAHEAD
+        lw t1, 0(s2)  #carica il PAHEAD del nodo testa, ossia l'indirizzo del nodo seguente...
+        mv s2,t1 #...che diventerà il nuovo nodo HEAD, quindi aggiorno HEAD_PTR
+        addi s4,s4,-1 #e decremento il counter dei nodi
+
+        beqz, s2, empty_list_update_lastptr #ovviamente se la lista è vuota, devo aggiornare anche LAST_PTR
+        j check_head_deletion #controlla se anche la nuova testa deve essere eliminata
+
+
+
+    empty_list_update_lastptr:
+
+        li s3,0 #metto a null il LAST_PTR
+        j end_DEL # se sono qui, allora non hai altri nodi da eliminare, termina
+
+    del_find_next:
+        mv t2,s2 #prosegui a partire dalla testa
+
+    del_find_loop:
+        addi t3,t2,1 # allinea al PAHEAD
+        lw t3,0(t3) #carica il puntatore al nodo successivo
+        beqz t3,end_DEL #e se non ce n'è uno termina
+
+        lb t4,0(t3) #carica il carattere (DATA) del nodo successivo
+        bne t4,a0,del_go_next # se il carattere non coincide, vai avanti
+
+        addi t5,t3,1 #allinea al PAHEAD del successivo  
+        lw t5, 0(t5) #carica l'indirizzo (tramite PAHEAD) del nodo successivo
+
+        addi t6,t2,1 #allinea al PAHEAD del nodo corrente
+        sw t5, 0(t6) #e scrivilo all'interno del PAHEAD nodo corrente
+
+        beq t3,s3,update_last_ptr #se il nodo è l'ultimo aggiorna il LAST_PTR
+
+        addi s4,s4,-1 #decrementa il contatore 
+        j del_find_loop
+
+
+    update_last_ptr:
+        mv s3,t2 #aggiorna LAST_PTR con l'ultimo indirizzo
+        addi s4,s4,-1 #decrementa il contatore
+        j del_find_loop
+
+
+    del_go_next:
+        mv t2,t3 #vado avanti
+        j del_find_loop
+
+
+    end_DEL:
+        lw ra,0(sp)
+        addi sp,sp,4
+        ret
+    
 
 
         
+
+
 find_next_free_addr: #ritorno in a1
     li t0,5 #devo trovare 5 byte
 
-check_bytes:
-    lb t1,0(s5) #leggo il byte corrente
-    bnez t1,reset_counter # se non trovo zero vuol dire che non c'è spazio
-    
-    addi t0,t0,-1 #decremento il contatore di ciclo (4 byte da analizzare, 3 byte da analizzare) 
-    beqz t0,found
+    check_bytes:
+        lb t1,0(s5) #leggo il byte corrente
+        bnez t1,reset_counter # se non trovo zero vuol dire che non c'è spazio
+        
+        addi t0,t0,-1 #decremento il contatore di ciclo (4 byte da analizzare, 3 byte da analizzare) 
+        beqz t0,found
 
-    addi s5,s5,1 # vado nel byte successivo
-    j check_bytes #ricontrollo
-    
-    
-    reset_counter:
-        addi s5,s5,1 #analizzo a partire dal bit successivo
-        li t0,5
-        j find_next_free_addr #continua finche non trovi 5 byte liberi
-    
-    found:
-        addi s5,s5,-4 # siccome sono andato avanti di 4 byte, vado indietro e ritorno
-        mv a1,s5
-        addi s5,s5,5 #siccome ora 5 byte sono occupati, faccio un grande balzo in avanti
-        
-        addi sp,sp,-4
-        sw a0,0(sp) #salvo  a0 nello stack
+        addi s5,s5,1 # vado nel byte successivo
+        j check_bytes #ricontrollo
         
         
-        # mv a0,a1 #DEBUG: MOSTRA INDIRIZZO
-        # li a7,34
-        # ecall
+        reset_counter:
+            addi s5,s5,1 #analizzo a partire dal bit successivo
+            li t0,5
+            j find_next_free_addr #continua finche non trovi 5 byte liberi
         
-        # li a0,10 #stampo un ritorno di linea
-        # li a7,11
-        # ecall
-        
-        lw a0,0(sp)
-        addi sp,sp,4 # ripristino la stack
-        
-        ret
+        found:
+            addi s5,s5,-4 # siccome sono andato avanti di 4 byte, vado indietro e ritorno
+            mv a1,s5
+            addi s5,s5,5 #siccome ora 5 byte sono occupati, faccio un grande balzo in avanti
+            
+            addi sp,sp,-4
+            sw a0,0(sp) #salvo  a0 nello stack
+            
+            
+            # mv a0,a1 #DEBUG: MOSTRA INDIRIZZO
+            # li a7,34
+            # ecall
+            
+            # li a0,10 #stampo un ritorno di linea
+            # li a7,11
+            # ecall
+            
+            lw a0,0(sp)
+            addi sp,sp,4 # ripristino la stack
+            
+            ret
 
 
 
